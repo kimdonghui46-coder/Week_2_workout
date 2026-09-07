cf01dbcfb4f6ab914f6ccebef67ab7f926d97598
kdh7546 <kimdonghui46@gmail.com>
2026-09-07 12:01:46 +0900

    역방향 피라미드 출력으로 수정

--- star.py
+++ star.py
@@ -7,7 +7,7 @@ class Pyramid:
             print(" " * (self.height - i) + "*" * (2 * i - 1))
 
 
-height = int(input("피라미드 높이를 입력하세요:5 "))
+height = int(input("피라미드 높이를 입력하세요: "))
 p = Pyramid(height)
 p.print_star_pyramid()
 
